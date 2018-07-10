394291778371baf52ada052737ee249fcf5fb2ee
Fabian Gülzau <guelzauf@sowi.hu-berlin.de>
2018-07-10 11:10:10 +0200

    Cleaning up code

--- analysis/Figures.R
+++ analysis/Figures.R
@@ -5,7 +5,7 @@
 if (!require("pacman")) install.packages("pacman")
 p_load(tidyverse, rio, countrycode, ggrepel)
 
-# Some plot tinkering
+# Participation rate of 0-to2-year-olds in ECEC, EU28, 2004-2013
 mean.enrolment <- ecec.df %>%
   filter(year >= 2004) %>%
   group_by(year) %>%
@@ -20,7 +20,7 @@ enrol.fig <- ggplot(ecec.plot.df, aes(x = year, y = enrolment)) +
   scale_y_continuous(name = "", labels = function(x) paste0(x*1, "%")) +
   labs(x = "",
        caption = "Source: OECD Family Database (Table PF3.2)") +
-  ggtitle(label = "Percentage of 0-to-2-year-olds enrolled in formal childcare and preschool institutions (EU28, 2004-2013)") +
+  ggtitle(label = "Percentage of 0-to-2-year-olds enrolled in formal childcare (EU28, 2004-2013)") +
   theme_minimal() +
   theme(panel.grid.minor.x = element_blank(),
         panel.grid.major.x = element_blank(),
@@ -37,5 +37,9 @@ enrol.fig <- enrol.fig + geom_text_repel(data = subset(ecec.plot.df,
                                                        Country %in% "Germany" & year == 2011),
                                          aes(label = Country), box.padding = 1)
 
-enrol.fig <- enrol.fig + geom_text_repel(data = subset(mean.enrolment, year == 2005), 
-                                         aes(label = label), box.padding = 1)
+enrol.fig <- enrol.fig + 
+  annotate(geom = "text", x = "2007", y = 22, label = "Mean of EU28") +
+  annotate(geom = "segment", x = "2008", xend = "2007", y = 29, yend = 23)
+  
+ggsave(filename = "./output/EnrolmentFigure.tiff", plot = enrol.fig, dpi = "print")
+
